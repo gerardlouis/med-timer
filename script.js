3d22bb3e05dea967959a849ca3b35c8d625672a7
Gerard <gerard.louis@dematic.com>
2025-02-13 23:01:46 -0500

    add done field to each medication log

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-medTimer = [];
+medTimer = {};
 interval = null;
-version = "1.3.0";
+version = "1.4.0";
 
 function setVersion() {
     document.getElementById("version").innerHTML = `Version: ${version}`;
@@ -11,24 +11,45 @@ function loadFromLocalStorage() {
 
     if (!medTimerLocalStorage || !JSON.parse(medTimerLocalStorage)) {
         // If medTimer is not found, initialize it with an empty array
-        localStorage.setItem('medTimer', JSON.stringify({ data: [] }));
-        medTimer = [];
+        medTimer = { data: [], doneFieldAdded: true };
+        localStorage.setItem('medTimer', JSON.stringify(medTimer));
     } else {
-        medTimer = JSON.parse(medTimerLocalStorage).data;
+        medTimer = JSON.parse(medTimerLocalStorage);
+        addDoneField();
     }
 }
 
 function saveToLocalStorage() {
-    localStorage.setItem('medTimer', JSON.stringify({ data: medTimer }));
+    localStorage.setItem('medTimer', JSON.stringify(medTimer));
 }
 
 function clearAll() {
     if (confirm('Are you sure you want to delete all medications?')) {
         localStorage.removeItem('medTimer');
-        medTimer = [];
+        medTimer = { data: [], doneFieldAdded: true };
         saveToLocalStorage();
         loadMedications();
         loadLastMedication();
+    }
+}
+
+function addDoneField() {
+    if (!medTimer.doneFieldAdded) {
+        const medTimerGrouped = Object.groupBy(medTimer.data, ({ name}) => name);
+
+        for (let name in medTimerGrouped) {
+            const sorted = medTimerGrouped[name].sort(compareMedication);
+
+            sorted.forEach((med, index) => {
+                if (index === 0) {
+                    med.done = false;
+                } else {
+                    med.done = true;
+                }
+            })
+        }
+
+        medTimer.doneFieldAdded = true;
     }
 }
 
@@ -37,14 +58,26 @@ function compareMedication(a, b) {
 
     const aDate = new Date(a.date + ' ' + a.time);
     const aTimeDiff = aDate.setHours(aDate.getHours() + parseInt(a.hours)) - now;
+    const aDone = a.done;
 
     const bDate = new Date(b.date + ' ' + b.time);
     const bTimeDiff = bDate.setHours(bDate.getHours() + parseInt(b.hours)) - now;
+    const bDone = b.done;
 
-    if (aTimeDiff < bTimeDiff) {
+    if (!aDone && !bDone) {
+        if (aTimeDiff < bTimeDiff) {
+            return -1;
+        }
+        if (aTimeDiff > bTimeDiff) {
+            return 1;
+        }
+        return 0;
+    }
+
+    if (!aDone && bDone) {
         return -1;
     }
-    if (aTimeDiff > bTimeDiff) {
+    if (aDone && !bDone) {
         return 1;
     }
     return 0;
@@ -57,8 +90,16 @@ function addMedication() {
     const time = form.elements["time"].value;
     const hours = form.elements["hours"].value;
 
-    const medication = { name, date, time, hours };
-    medTimer.unshift(medication);
+    const medication = { name, date, time, hours, done: false };
+    medTimer.data.filter(item => {
+        if (item.name === name && item.done === false) {
+            return true;
+        }
+        return false;
+    }).forEach(med => {
+        med.done = true;
+    });
+    medTimer.data.unshift(medication);
     saveToLocalStorage();
     loadMedications();
     loadLastMedication();
@@ -66,7 +107,17 @@ function addMedication() {
 
 function deleteMedication(index) {
     if (confirm('Are you sure you want to delete this medication?')) {
-        medTimer.splice(index, 1);
+        const name = medTimer.data[index].name;
+        medTimer.data.splice(index, 1);
+        const previous = medTimer.data.filter(item => {
+            if (item.name === name && item.done === false) {
+                return true;
+            }
+            return false;
+        }).sort(compareMedication);
+        if (previous) {
+            previous.done = false;
+        }
         saveToLocalStorage();
         loadMedications();
         loadLastMedication();
@@ -80,7 +131,7 @@ function loadLastMedication() {
 
     interval = setInterval(() => {
         const now = new Date();
-        const lastMedication = medTimer.sort(compareMedication)[0];
+        const lastMedication = medTimer.data.filter(item => item.done === false).sort(compareMedication)[0];
 
         if (!lastMedication) {
             clearInterval(interval);
@@ -112,7 +163,7 @@ function loadMedications() {
     const container = document.getElementById("medications");
     container.innerHTML = "";
 
-    if (medTimer.length === 0) {
+    if (medTimer.data.length === 0) {
         return;
     }
 
@@ -121,15 +172,16 @@ function loadMedications() {
     const tableBody = document.createElement("tbody");
     const headerRow = document.createElement("tr");
     tableHeader.appendChild(headerRow);
-    headerRow.innerHTML = `<th>Name</th><th>Date/Time</th><th class="hide">Interval (h)</th><th>X</th>`;
+    headerRow.innerHTML = `<th class="delete"></th><th>Name</th><th>Date/Time</th><th class="hide">Interval (h)</th><th>Taken?</th>`;
     table.appendChild(tableHeader);
-    medTimer.sort(compareMedication).forEach((medication, index) => {
+    medTimer.data.sort(compareMedication).forEach((medication, index) => {
         const row = document.createElement("tr");
         row.innerHTML = `
+            <td class="delete"><button type="button" onclick="deleteMedication(${medication.index})" title="Delete?">X</button></td>
             <td>${medication.name}</td>
             <td>${medication.date}<br>${medication.time}</td>
             <td class="hide">${medication.hours}</td>
-            <td><button type="button" onclick="deleteMedication(${medication.index})">Delete</button></td>
+            <td>${medication.done ? 'Yes' : 'No'}</td>
         `;
         tableBody.appendChild(row);
     });
